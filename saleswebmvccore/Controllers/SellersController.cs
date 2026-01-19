0b22c13a7d8a8e62f15c3e554bde59ee2b710228
Diego Gomes <diegogns18@outlook.com>
2026-01-19 14:33:16 -0300

    SellerFormViewModel and Department select component

--- saleswebmvccore/Controllers/SellersController.cs
+++ saleswebmvccore/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using saleswebmvccore.Models;
+using saleswebmvccore.Models.ViewModels;
 using saleswebmvccore.Services;
 using System;
 using System.Collections.Generic;
@@ -11,10 +12,12 @@ namespace saleswebmvccore.Controllers
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +28,9 @@ namespace saleswebmvccore.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
--- saleswebmvccore/Services/DepartmentService.cs
+++ saleswebmvccore/Services/DepartmentService.cs
@@ -7,24 +7,18 @@ using System.Threading.Tasks;
 
 namespace saleswebmvccore.Services
 {
-    public class SellerService
+    public class DepartmentService
     {
         private readonly saleswebmvccoreContext _context;
 
-        public SellerService(saleswebmvccoreContext context)
+        public DepartmentService(saleswebmvccoreContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public List<Department> FindAll()
         {
-            return _context.Seller.ToList();
-        }
-
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
+            return _context.Department.OrderBy(x => x.Name).ToList();
         }
     }
 }
