983c77acb6e508ef7416336985a98c31c812ed9e
Diego Gomes <diegogns18@outlook.com>
2026-01-19 14:04:42 -0300

    Foreign key not null (referential integrity)

--- saleswebmvccore/Services/SellerService.cs
+++ saleswebmvccore/Services/SellerService.cs
@@ -23,6 +23,7 @@ namespace saleswebmvccore.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
