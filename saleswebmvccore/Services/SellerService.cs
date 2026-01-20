2b925eb22db8f6b940d8d40da48cd72c2675cc17
Diego Gomes <diegogns18@outlook.com>
2026-01-20 16:23:47 -0300

    Update seller and custom service exception

--- saleswebmvccore/Services/SellerService.cs
+++ saleswebmvccore/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using saleswebmvccore.Services.Exceptions;
 
 namespace saleswebmvccore.Services
 {
@@ -38,6 +39,23 @@ namespace saleswebmvccore.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller obj)
+        {
+            if(!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
