181d1b82ae1d9653815a8f8572942c6f9bdbc5a3
Diego Gomes <diegogns18@outlook.com>
2026-01-29 15:31:54 -0300

    Asynchronous operations using Tasks (async, await)

--- saleswebmvccore/Services/DepartmentService.cs
+++ saleswebmvccore/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using saleswebmvccore.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using saleswebmvccore.Data;
 using saleswebmvccore.Models;
 using System;
 using System.Collections.Generic;
@@ -16,9 +17,9 @@ namespace saleswebmvccore.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(x => x.Name).ToList();
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();
         }
     }
 }
